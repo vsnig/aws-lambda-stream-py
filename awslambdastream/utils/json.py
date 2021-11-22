d2f96043296f979f21b87cce746e669111bbe016
Ivan <4790093+snegostup@users.noreply.github.com>
2021-11-22 18:35:16 +0200

    add json numpyencoder

--- awslambdastream/utils/json.py
+++ awslambdastream/utils/json.py
@@ -1,15 +1,15 @@
 import json
-
+from numpyencoder import NumpyEncoder
 
 # custom encoder to properly encode b'bytes'
 #   https://programmerah.com/solved-typeerror-object-of-type-bytes-is-not-json-serializable-32328/
 #   https://stackoverflow.com/questions/40000495/how-to-encode-bytes-in-json-json-dumps-throwing-a-typeerror
-class MyEncoder(json.JSONEncoder):
+class MyEncoder(NumpyEncoder):
     def default(self, obj):
         if isinstance(obj, bytes):
             return str(obj, encoding="ascii")
         else:  # pragma: no cover
-            return json.JSONEncoder.default(self, obj)
+            return NumpyEncoder.default(self, obj)
 
 
 def json_dumps(obj):
