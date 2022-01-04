17e0b89dafbf8041b47a5e16a07a8fa0eb377ac9
Ivan <4790093+snegostup@users.noreply.github.com>
2022-01-04 22:26:17 +0200

    use AWS_REGION in eventbus

--- awslambdastream/connectors/eventbridge.py
+++ awslambdastream/connectors/eventbridge.py
@@ -1,10 +1,11 @@
+import os
 import boto3
 from botocore.config import Config
 
 
 class EventBridgeConnector:
     def __init__(self, *, logger, timeout=1000, **client_opts):
-        config = Config(read_timeout=timeout)
+        config = Config(read_timeout=timeout, region_name=os.environ.get("AWS_REGION"))
         self.bus = boto3.client("events", config=config, **client_opts)
         self.logger = logger
 
