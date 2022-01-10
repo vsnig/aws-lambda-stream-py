4ae3614bafe0043b89b32bf7d77fa44d4fbce383
Ivan <4790093+snegostup@users.noreply.github.com>
2022-01-10 16:31:35 +0200

    added logger to default_options

--- awslambdastream/utils/opt.py
+++ awslambdastream/utils/opt.py
@@ -1,5 +1,10 @@
 import os
+import logging
 
 from awslambdastream.utils.eventbridge import publish_to_eventbridge
 
-default_options = {"bus_name": os.getenv("BUS_NAME"), "publish": publish_to_eventbridge}
+default_options = {
+    "logger": logging.getLogger("handler"),
+    "bus_name": os.getenv("BUS_NAME"),
+    "publish": publish_to_eventbridge,
+}
